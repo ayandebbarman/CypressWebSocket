a0816dd8f4984048bc8067d8f9f1d122ed400611
Deb Barman <z988447@thryv.com>
2019-12-20 10:56:19 +0530

    first commit

---
+++ cypress/integration/rediff2.spec.js
@@ -0,0 +1,9 @@
+describe('first test',()=>{
+
+it('should launch feelaplace',()=>{
+
+    cy.visit('https://www.rediff.com')
+
+})
+
+})
---
+++ cypress/integration/rediff5.spec.js
@@ -0,0 +1,9 @@
+describe('first test',()=>{
+
+it('should launch rediff',()=>{
+
+    cy.visit('https://www.rediff.com')
+
+})
+
+})
